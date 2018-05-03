cf995907dd70febf0bdfad9671426dc3f7f6140b
Peter <peter.wieringa@gmail.com>
2018-05-03 20:54:19 +0200

    initial commit

--- config_updater.py
+++ config_updater.py
@@ -3,24 +3,33 @@ from travispy import TravisPy
 import os
 import git
 
-# Create a Travis access token with the following scopes:
-# read:org
-# user:email
-# repo_deployment
-# repo:status
-# write:repo_hook
+# ##############################################################################################################
+#                                                                                                              #
+# Python script to check for a new build off your HomeAssistant config, pull repo and restart docker container #
+#                                                                                                              #
+# Create a Travis access token with the following scopes:                                                      #
+# read:org                                                                                                     #
+# user:email                                                                                                   #
+# repo_deployment                                                                                              #
+# repo:status                                                                                                  #
+# write:repo_hook                                                                                              #
+################################################################################################################
 
 # config start
 
+# file to store latest build number from Travis
 file = '/tmp/last_build.txt'
 
+# git
 git_dir = '/home/hass/home-assistant'
 g = git.cmd.Git(git_dir)
 
+# travis
 github_token = '<your github token>'
 t = TravisPy.github_auth(github_token)
 repo = t.repo('<your github repo name>')
 
+# docker
 ha_container_name = '<home-assistant containername>'
 # config end
 
@@ -30,6 +39,9 @@ cli = Client(base_url='unix://var/run/docker.sock')
 build_status = t.build(repo.last_build_id).state
 latest_build = t.build(repo.last_build_id).number
 
+# config end
+
+# read latest buildnumber from file, if file does not exist create it with buildnumber = 0
 try:
     fo = open(file, 'r+')
     current_build = fo.read(10)
@@ -38,6 +50,7 @@ except:
     fo = open(file, 'wb')
     fo.write(0 + '\n')
 
+# check buildnumber and status against current buildnumber, pull, restart container or do nothing
 if int(current_build) < int(latest_build) and build_status = 'passed':
     print('Current build is: ' + current_build + 'Newer build '+ latest_build + ' available, pulling reposistory')
     g.pull()
